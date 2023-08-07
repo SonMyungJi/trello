f6fb35271389e1df1edcbef77be7eedbf2cee364
Lee hyunkyoung <hyunk.office@gmail.com>
2023-08-07 14:51:01 +0900

    Column Entity init

---
+++ src/main/java/com/example/trello/entity/Column.java
@@ -0,0 +1,22 @@
+package com.example.trello.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "column")
+@NoArgsConstructor
+public class Column {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @jakarta.persistence.Column(name = "column_id" )
+    private Long columnId;
+
+    @jakarta.persistence.Column(name = "column_name", nullable = false, unique = true)
+    private String columnName;
+}
