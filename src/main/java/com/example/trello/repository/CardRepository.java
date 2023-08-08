8ebd498bafcdd531d64e9302b2a1bca3d1039603
gunwooknam2023 <132857666+gunwooknam2023@users.noreply.github.com>
2023-08-08 15:59:13 +0900

    Delete CardRepository.java

--- src/main/java/com/example/trello/repository/CardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.trello.repository;
-
-import com.example.trello.entity.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardRepository extends JpaRepository<Card, Long> {
-}
