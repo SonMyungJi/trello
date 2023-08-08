78791a7039f631664494906e40f43bffb4cbfb62
gunwooknam2023 <132857666+gunwooknam2023@users.noreply.github.com>
2023-08-08 15:59:19 +0900

    Delete CommentRepository.java

--- src/main/java/com/example/trello/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.trello.repository;
-
-import com.example.trello.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
