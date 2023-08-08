3a50476dcd5ad8d27d5c8a34949611fe3454e1f2
gunwooknam2023 <132857666+gunwooknam2023@users.noreply.github.com>
2023-08-08 15:59:07 +0900

    Delete ColumnRepository.java

--- src/main/java/com/example/trello/repository/ColumnRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.trello.repository;
-
-import com.example.trello.entity.ColumnEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ColumnRepository extends JpaRepository<ColumnEntity, Long> {
-}
