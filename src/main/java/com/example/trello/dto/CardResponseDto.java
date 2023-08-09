9aaeb6565f8e4b51d4d97edff9f7c8b24cc1edff
SonMyungJi <linolousky@gmail.com>
2023-08-09 12:17:51 +0900

    card 생성시 객체를 받아옴

--- src/main/java/com/example/trello/dto/CardResponseDto.java
+++ src/main/java/com/example/trello/dto/CardResponseDto.java
@@ -2,10 +2,13 @@ package com.example.trello.dto;
 
 import com.example.trello.entity.Card;
 import com.example.trello.entity.Comment;
+import com.example.trello.entity.Section;
+import com.example.trello.entity.User;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Getter
@@ -15,6 +18,9 @@ public class CardResponseDto {
     private String cardName;
     private String cardDesc;
     private String cardColor;
+    private User user;
+    private Section section;
+    private Date dueDate;
     private List<CommentResponseDto> commentList = new ArrayList<>();
 
     public CardResponseDto(Card card) {
@@ -24,5 +30,8 @@ public class CardResponseDto {
         for (Comment comment : card.getComments()) {
             commentList.add(new CommentResponseDto(comment));
         }
+        this.user = user.getUser(card.getWorkerId());
+        this.section = card.getSection();
+        this.dueDate = card.getDueDate();
     }
 }
