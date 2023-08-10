9aba4a9f46041ab9485953e1ccc049771afa41b6
SonMyungJi <linolousky@gmail.com>
2023-08-10 23:10:43 +0900

    edit에서 기능 설정

--- src/main/java/com/example/trello/controller/UserViewController.java
+++ src/main/java/com/example/trello/controller/UserViewController.java
@@ -6,27 +6,40 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api/view/user")
 public class UserViewController {
 
-    @GetMapping("/login")
-    public String loginPage() {
-        return "login";
-    }
+  @GetMapping("/login")
+  public String loginPage() {
+    return "login";
+  }
 
-    @GetMapping("/signup")
-    public String signupPage() { return "signup";}
+  @GetMapping("/signup")
+  public String signupPage() {
+    return "signup";
+  }
 
-    @GetMapping("/profile")
-    public String updatePage(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails)
-            throws JsonProcessingException {
+  @GetMapping("/profile")
+  public String updatePage(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails)
+      throws JsonProcessingException {
 
-        model.addAttribute("info_username",userDetails.getUser().getUsername());
-        model.addAttribute("info_user",userDetails.getUser());
-        return "profile";
-    }
+    model.addAttribute("info_username", userDetails.getUser().getUsername());
+    model.addAttribute("info_user", userDetails.getUser());
+    return "profile";
+  }
+
+  @GetMapping("/boards/{boardId}")
+  public String homePage(@PathVariable Long boardId) {
+    return "readOnly";
+  }
+
+  @GetMapping("/boards/{boardId}/edit")
+  public String editPage(@PathVariable Long boardId) {
+    return "edit"; // edit.html 파일이 위치한 경로로 수정해야 합니다.
+  }
 }
 
