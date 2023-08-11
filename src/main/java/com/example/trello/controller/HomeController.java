38626e6ab06c3305df86b233c38407b5bf5bc715
mbc2579 <mbc2579@naver.com>
2023-08-11 17:57:44 +0900

    구글 로그인

--- src/main/java/com/example/trello/controller/HomeController.java
+++ src/main/java/com/example/trello/controller/HomeController.java
@@ -1,18 +1,13 @@
 package com.example.trello.controller;
 
-import com.example.trello.jwt.JwtUtil;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "index";
+        return "columns";
     }
 
 //    @GetMapping("/")
--- src/main/java/com/example/trello/controller/UserViewController.java
+++ src/main/java/com/example/trello/controller/UserViewController.java
@@ -31,5 +31,8 @@ public class UserViewController {
         return "profile";
     }
 
-
+    @GetMapping("/boards/{boardId}")
+    public String readOnlyPage() {
+        return "readOnly";
+    }
 }
